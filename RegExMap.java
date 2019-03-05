079425496a85005761cd81d2c77bc174c2d3be5e
Jan Sebek <41228202+Catarang1@users.noreply.github.com>
2019-03-05 09:03:21 +0100

    Update RegExMap.java

--- RegExMap.java
+++ RegExMap.java
@@ -1,32 +1,36 @@
+package commons;
 import java.util.*;
 
-public class Test {
-    public static void main (String[] parameters) {
-        Map map = new RegExMap ();
+public class RegExMap<K, V> extends HashMap<K, V> {
 
-        map.put ("foo(.+)", "value");
+	@Override
+	public V get(Object key) {
 
-        System.out.println (map.get ("foo"));
-        System.out.println (map.get ("foobar"));
-    }
+		String keyAsString = key.toString();
+		String regExKey = null;
+		Iterator it = keySet().iterator();
+		while (it.hasNext() && (regExKey == null)) {
+			String keyFromSet = it.next().toString();
+			if (keyAsString.matches(keyFromSet)) 
+				regExKey = keyFromSet;
+		}
+		return (regExKey == null) ? null : super.get(regExKey);
 
-    private static class RegExMap extends HashMap {
-        public Object get (Object key) {
-            String keyAsString = key.toString ();
+	}
 
-            String regExKey = null;
-
-            Iterator it = keySet ().iterator ();
-
-            while (it.hasNext () && (regExKey == null)) {
-                String keyFromSet = it.next ().toString ();
-
-                if (keyAsString.matches (keyFromSet)) {
-                    regExKey = keyFromSet;
-                }
-            }
-
-            return (regExKey == null) ? null : super.get (regExKey);
-        }
-    }
+	@Override
+	public boolean containsKey(Object key) {
+		String keyAsString = key.toString();
+		Iterator it = keySet().iterator();
+		Boolean result = null;
+		
+		while (it.hasNext() && (result == null)) {
+			String keyFromSet = it.next().toString();
+			if (keyAsString.matches(keyFromSet)) {
+				result = true;
+				break;
+			}
+		}
+		return (result == null) ? false : true;
+	}
 }
